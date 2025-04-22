902efb5af1ce27281ec65a53e02bb52fd8eb63b6
peternaylor <peter@oldtiogafarm.com>
2025-04-22 11:54:43 -0400

    linux functionality + windows dirchanging

--- Makefile
+++ Makefile
@@ -1,16 +1,23 @@
-CC=x86_64-w64-mingw32-gcc
-SRC=src/main
-DEPS=src/headers
+CC=gcc
+WINC=x86_64-w64-mingw32-gcc
+SRC=src/linux/main
+WSRC=src/windows/main
+DEPS=src/linux/headers
+WDEPS=src/windows/headers
 TARGETS=targets
 SRS=$(SRC)/airfryer.c $(SRC)/fencrypt.c $(SRC)/fdecrypt.c $(SRC)/iterated.c
+WSRS=$(WSRC)/airfryer.c $(WSRC)/fencrypt.c $(WSRC)/fdecrypt.c $(WSRC)/iterated.c
 
-airfryer.exe: $(SRS) $(TARGETS)
+airfryer: $(SRC) $(TARGETS)
 	$(CC) -o $@ $(SRS) -I $(DEPS)
+win: $(WSRS) $(TARGETS)
+	$(WINC) -o airfryer.exe $(WSRS) -I $(WDEPS)
 $(TARGETS):
 	if ! [ -d $(TARGETS) ]; then 		\
 		mkdir $(TARGETS);		\
 	fi
 	- cp * -r $(TARGETS)
 clean:
-	rm -rf *.exe
+	rm -f airfryer
+	rm -f *.exe
 	rm -rf $(TARGETS)
---
+++ src/windows/main/airfryer.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <windows.h>
+#include "airf.h"
+
+#ifndef EXIT_FAILURE
+#define EXIT_FAILURE 1
+#endif
+
+int main(int argc, char *argv[]) {
+
+	char toexec[50];
+
+	if (argc > 2) 
+		return EXIT_FAILURE;
+	if (argc == 1) {
+		printf("%s\n", "Launch!");
+		if (!SetCurrentDirectory("C:\\")) {
+			fprintf(stderr, "SetCurrentDirectory error : %d\n", GetLastError());
+			return EXIT_FAILURE;
+		}
+		if (sprintf(toexec, "%s %s", argv[0], "-e") == -1) {
+			perror("sprintf err");
+			exit(EXIT_FAILURE);
+		}
+		iterate_dir("-e", toexec);
+	} else {
+		sprintf(toexec, "%s %s", argv[0], argv[1]) {
+			perror("sprintf err");
+			exit(EXIT_FAILURE);
+		}
+		iterate_dir(argv[1], toexec);
+	}
+	return 0;
+
+}
